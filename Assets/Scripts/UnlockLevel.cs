bc6f5f2c060fa31a67d41d4ac805c025d85ec01e
Leqi <100080774+toodemhard@users.noreply.github.com>
2024-12-16 23:18:31 +0000

    zzzzz

--- Assets/Scripts/UnlockLevel.cs
+++ Assets/Scripts/UnlockLevel.cs
@@ -4,14 +4,17 @@ public class UnlockLevel : MonoBehaviour
 {
     [SerializeField]
     Robot robot;
-    public int levelToUnlock; // The level this trigger will unlock
+    [SerializeField]
+    int levelToUnlock; // The level this trigger will unlock
     public static bool[] levelsUnlocked = new bool[4] { true, false, false,false }; // Level 1 is always unlocked
 
     private void OnTriggerEnter(Collider other)
     {
        
         levelsUnlocked[levelToUnlock - 1] = true; // Unlock the specific level
-        robot.isRunning = false;
+        if (robot != null) {
+            robot.isRunning = false;
+        }
 
         Debug.Log("Level " + levelToUnlock + " unlocked by " + other.name);
     }
