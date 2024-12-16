984df04d6ba764ba35a4f30f35fa3f5742417719
Leqi <100080774+toodemhard@users.noreply.github.com>
2024-12-16 19:05:50 +0000

    int and bool expressions

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,30 +6,44 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     float MinInterval = 2;
+    [SerializeField]
     float MaxInterval = 5;
 
     float offDuration = 3;
+    float nextOffTime;
+    float accumulator = 0;
 
-
+    public bool On = true;
     MeshRenderer meshRenderer;
 
-    void Start() {
-        
+    void Awake() {
+        meshRenderer = GetComponent<MeshRenderer>();
+        NextOffTime();
     }
 
     void NextOffTime() {
-
+        nextOffTime = Random.Range(MinInterval, MaxInterval);
     }
+
     void Update() {
-        if (gameObject.activeSelf) {
-
+        accumulator += Time.deltaTime;
+        if (On) {
+            if (accumulator >= nextOffTime) {
+                On = false;
+                meshRenderer.enabled = false;
+                accumulator = 0;
+            }
         } else {
-
+            if (accumulator >= offDuration) {
+                On = true;
+                meshRenderer.enabled = true;
+                accumulator = 0;
+            }
         }
     }
 
     void OnTriggerEnter(Collider collider) {
-        if (collider.CompareTag("Robot")) {
+        if (On == collider.CompareTag("Robot")) {
             collider.gameObject.SetActive(false);
         }
     }
