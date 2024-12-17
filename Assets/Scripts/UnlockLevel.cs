9c028150558f9973994a0a8a379b6b0c36071d1b
Leqi <100080774+toodemhard@users.noreply.github.com>
2024-12-17 01:15:45 +0000

    unlocked text

--- Assets/Scripts/UnlockLevel.cs
+++ Assets/Scripts/UnlockLevel.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class UnlockLevel : MonoBehaviour
@@ -6,7 +7,16 @@ public class UnlockLevel : MonoBehaviour
     Robot robot;
     [SerializeField]
     int levelToUnlock; // The level this trigger will unlock
+
+    [SerializeField]
+    TMP_Text text;
     public static bool[] levelsUnlocked = new bool[4] { true, false, false,false }; // Level 1 is always unlocked
+
+    private void Awake() {
+        if (text != null) {
+            text.gameObject.SetActive(false);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +24,16 @@ public class UnlockLevel : MonoBehaviour
         levelsUnlocked[levelToUnlock - 1] = true; // Unlock the specific level
         if (robot != null) {
             robot.isRunning = false;
+
+        }
+
+        if (text != null) {
+            if (levelToUnlock == levelsUnlocked.Length - 1) {
+                text.text = $"Credits Unlocked";
+            } else {
+                text.text = $"Level {levelToUnlock} Unlocked!";
+            }
+            text.gameObject.SetActive(true);
         }
 
         Debug.Log("Level " + levelToUnlock + " unlocked by " + other.name);
